651d55e39b332469207ee00fa9727c26f0039bdc
Mekhpule <mekhpule@gmail.com>
2020-04-24 15:53:33 -0400

    add Formatting.java

---
+++ src/day_1/Formatting.java
@@ -0,0 +1,8 @@
+package day_1;
+
+public class Formatting {
+    public static void main(String[] args) {
+        System.out.println("We learned a lot about Date and time formatting in Java");
+
+    }
+}
