e3e393efdada40ccca0d0a6057b0c08d380bc2ec
mekhpule <64181630+mekhpule@users.noreply.github.com>
2020-05-08 01:25:58 -0400

    Update HappyBirthday.java

--- src/day_1/HappyBirthday.java
+++ src/day_1/HappyBirthday.java
@@ -2,7 +2,8 @@ package day_1;
 
 public class HappyBirthday {
     public static void main(String[] args) {
-        System.out.println("Happy Birthday to you!");
+        System.out.println("Happy Birthday to you Batch 18!");
+        System.out.println("This is apractice")
 
     }
 }
